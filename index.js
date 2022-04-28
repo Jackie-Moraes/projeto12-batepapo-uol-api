25c60b919c49e6d8863fed9eb6b15136e5c18c07
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-04-28 20:53:03 -0300

    post & get participants (no joi)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
-import dotenv from "dotenv";
+import dayjs from "dayjs";
+import 'dayjs/locale/pt-br.js'
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
@@ -12,9 +14,51 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db = null;
 
 const promise = mongoClient.connect();
-
 promise.then(() => {
     db = mongoClient.db("batepapo_uol")
+})
+promise.catch((e) => {
+    console.log("Algo deu errado...", e)
+})
+
+app.get('/participants', async (req, res) => {
+    try {
+        await mongoClient.connect();
+        const participants = await db.collection('participants').find({}).toArray();
+
+        res.send(participants);
+    } catch (e) {
+        console.log("Erro!", e);
+        res.status(500).send("Algo deu errado.", e);
+    } finally {
+        mongoClient.close();
+    }
+})
+
+app.post('/participants', async (req, res) => {
+    const participant = req.body.name;
+    const exists = db.collection('participants').findOne({name: participant})
+
+    if (exists) {
+        res.status(409).send("Esse nome já está em uso.");
+    }
+
+    if (typeof participant != "string" || participant.length < 1) {
+        res.sendStatus(422);
+    }
+
+    try {
+        await mongoClient.connect();
+        await db.collection('participants').insertOne({name: participant, lastStatus: Date.now()});
+        // const now = dayjs().format('HH:mm:ss');
+        await db.collection('messages').insertOne({from: participant, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')});
+        res.sendStatus(201);
+    } catch (e) {
+        console.log("Erro!", e);
+        res.status(500).send("Algo deu errado.", e);
+    } finally {
+        mongoClient.close();
+    }
 })
 
 app.listen(process.env.PORTA, () => console.log("Server running!")); 
